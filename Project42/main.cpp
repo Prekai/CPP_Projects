a201152db59f51557f7f8d4c2eaad1b53b5eee15
Hugyecz Arnót <arnothugyecz@gmail.com>
2023-03-21 16:16:56 +0100

    It is not my solution, I do not really understand how to built piramids, it seems I had a good thought but I can not go further. So I started search for solution. I will be okay.

--- Project42/main.cpp
+++ Project42/main.cpp
@@ -139,8 +139,13 @@ int main() {
 //        cout << endl;    
 //    }
 
-
 //    // pyramid with *
+//    //        *
+//    //      * * *
+//    //    * * * * *
+//    //  * * * * * * *
+//    //* * * * * * * * *
+
 //    int rows {};
 //    cout << "enter number of rows: ";
 //    cin >> rows;
@@ -180,8 +185,36 @@ int main() {
     //    3 4 5 4 3
     //  4 5 6 7 6 5 4
     //5 6 7 8 9 8 7 6 5
-    
-    
+
+
+    int rows, count = 0, count1 = 0, k = 0;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for(int i = 1; i <= rows; ++i) {
+        for(int space = 1; space <= rows-i; ++space) {
+            cout << "  ";
+            ++count;
+        }
+
+        while(k != 2*i-1) {
+            if (count <= rows-1) {
+                cout << i+k << " ";
+                ++count;
+            }
+            else {
+                ++count1;
+                cout << i+k-2*count1 << " ";
+            }
+            ++k;
+        }
+        count1 = count = k = 0;
+
+        cout << endl;
+    }
+
+
 
 
 	cout << endl;

4067b5c13a30a4728019ab1886c91f7aad0eef9e
Hugyecz Arnót <arnothugyecz@gmail.com>
2023-03-23 12:05:01 +0100

    I finally could solve the pyramid program, I learned a lot about loops, I enjoyed the challenge

--- Project42/main.cpp
+++ Project42/main.cpp
@@ -1,39 +1,35 @@
 //Create a letter pyramid, what is based in input
-//
-//   A
-//  ABA
-// ABCBA
-//ABCDCBA
-
 #include <iostream>
 #include <string>
-#include <cstring>
 
 using namespace std;
 
 int main() {
     
-//    cout << "I will make you a letter pyramid, please enter your characters: ";
-//    
-//    string letter_pyramid {};
-//    string space = "space";
-//    size_t i {};
-//    
-//    getline(cin, letter_pyramid);
-//    
-//    for (; i<letter_pyramid.size(); ++i)
-//        if (i >=0)
-//            cout << i*space;
-//            cout << i;
-//            
-//    for (auto c:letter_pyramid)
-//        cout << c << endl;
-//    
+    cout << "I will make you a letter pyramid, please enter your characters: ";
+    string letter_pyramid {};
+    getline(cin,letter_pyramid);
+    
+    for(size_t i {1}; i<=letter_pyramid.size(); i++){
+        for(size_t j = 1;  j<=letter_pyramid.size()-i; j++){
+            cout << " ";
+        }
+        int m {0};
+        for(size_t k {0} ; k<i  ;k++, m++){
+            cout << letter_pyramid.at(m);
+        }
+        m-=2;
+        for(size_t l; 0<=m; l++, m--){
+            cout << letter_pyramid.at(m);
+        }
+        cout << endl;
+    }
+     
 
 
 //*********************************************************************************************************************************************
 // ********************************************************************************************************************************************
-// faild to create letter piramid based on the requiremnt, no problem I try to solve different puzzles and I will get back to the original project
+// faild to create letter piramid based on the requirement, no problem I try to solve different puzzles and I will get back to the original project
 
 
 
@@ -91,7 +87,7 @@ int main() {
 //   
 
 
-//    //  inverted rows with *
+////  inverted rows with *
 //    int rows {};
 //    cout << "enter number of rows: ";
 //    cin >> rows;
@@ -179,40 +175,68 @@ int main() {
 //    }
 
 
-    //number pyramid
-    //        1
-    //      2 3 2
-    //    3 4 5 4 3
-    //  4 5 6 7 6 5 4
-    //5 6 7 8 9 8 7 6 5
+//    //number pyramid
+//    //        1
+//    //      2 3 2
+//    //    3 4 5 4 3
+//    //  4 5 6 7 6 5 4
+//    //5 6 7 8 9 8 7 6 5
 
 
-    int rows, count = 0, count1 = 0, k = 0;
+//    int rows, count = 0, count1 = 0, k = 0;
+//
+//    cout << "Enter number of rows: ";
+//    cin >> rows;
+//
+//    for(int i = 1; i <= rows; ++i) {
+//        for(int space = 1; space <= rows-i; ++space) {
+//            cout << "  ";
+//            ++count;
+//        }
+//
+//        while(k != 2*i-1) {
+//            if (count <= rows-1) {
+//                cout << i+k << " ";
+//                ++count;
+//            }
+//            else {
+//                ++count1;
+//                cout << i+k-2*count1 << " ";
+//            }
+//            ++k;
+//        }
+//        count1 = count = k = 0;
+//
+//        cout << endl;
+//    }
 
-    cout << "Enter number of rows: ";
-    cin >> rows;
+//    //number pyramid or letter pyramid just replace the char m='A' to int m {1}
+//    //        1
+//    //      1 2 1
+//    //    1 2 3 2 1
+//    //  1 2 3 4 3 2 1
+//    //1 2 3 4 5 4 3 2 1
 
-    for(int i = 1; i <= rows; ++i) {
-        for(int space = 1; space <= rows-i; ++space) {
-            cout << "  ";
-            ++count;
-        }
+//    int rows {0};
+//
+//    cout << "Enter number of rows: ";
+//    cin >> rows;
+//    
+//    for(int i {1}; i<=rows; i++){
+//        for(int j = 1;  j<=rows-i; j++){
+//            cout << "  ";   
+//        }        
+//        char m = 'A';
+//        for(int k {1}; k<=i; k++,m++){
+//            cout << m << " ";
+//        }
+//        m-=2;
+//        for(int l {1}; l<i  ;l++, m--)
+//            cout << m << " ";
+//        cout << endl;
+//    }     
 
-        while(k != 2*i-1) {
-            if (count <= rows-1) {
-                cout << i+k << " ";
-                ++count;
-            }
-            else {
-                ++count1;
-                cout << i+k-2*count1 << " ";
-            }
-            ++k;
-        }
-        count1 = count = k = 0;
 
-        cout << endl;
-    }
 
 
 
